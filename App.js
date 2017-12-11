1071c7379e7810e3bc5880c09cde39abbfce10b2
James-Mission <james-mission@pluralsight.com>
2017-12-11 14:59:27 -0800

    Adding camera functionality

--- App.js
+++ App.js
@@ -9,29 +9,27 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  AppRegistry,
+  Button
 } from 'react-native';
+import ImagePicker from 'react-native-image-crop-picker';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
+export default class secretSnowFlake extends Component {
+  openPicker(){
+  ImagePicker.openCamera({
+    width: 300,
+    height: 300,
+    cropping: true
+  })
+}
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Button
+      onPress={ () => this.openPicker() }
+      title={ "Change Picture"}
+      />
       </View>
     );
   }
@@ -55,3 +53,5 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+AppRegistry.registerComponent('secretSnowFlake', () => secretSnowFlake)
